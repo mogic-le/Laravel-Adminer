d59d7934055630531e45e044427fa3c815024909
Stefan Berger <berger@mogic.com>
2020-01-04 22:23:05 +0100

    change to latest version and add some fixes

---
+++ src/adminer-sqlite-login.php
@@ -0,0 +1,15 @@
+<?php
+if (!function_exists("adminer_object"))
+{
+    function adminer_object()
+    {
+        class AdminerSqliteLogin extends Adminer
+        {
+            public function login($ce, $G)
+            {
+                return true;
+            }
+        }
+        return new AdminerSqliteLogin;
+    }
+}
